a6dd399bcf7b3f85e0ea15ea0cdc09daa28a3a9f
Bastien <nisole.bastien@outlook.fr>
2020-02-19 23:03:52 +0100

    [IHM] - Start of IHM

---
+++ Toolfus/App.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace Toolfus
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App
+    {
+    }
+}
---
+++ Toolfus/MainWindow.xaml.cs
@@ -0,0 +1,13 @@
+﻿namespace Toolfus
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
