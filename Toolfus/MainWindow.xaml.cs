e3685098061f7e10eb03da19c049f5010116ecdb
Bastien <nisole.bastien@outlook.fr>
2020-02-22 11:06:45 +0100

    [IHM] - Get openned dofus windows

--- Toolfus/MainWindow.xaml.cs
+++ Toolfus/MainWindow.xaml.cs
@@ -1,16 +1,39 @@
-﻿using System.Windows;
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Windows;
 using WpfApplication1;
+using System.Diagnostics;
+using System.Windows.Controls;
 
 namespace Toolfus
 {
+    
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
     public partial class MainWindow
     {
+        List<Process> dofus = new List<Process>();
+
         public MainWindow()
         {
             InitializeComponent();
+            Process[] processlist = Process.GetProcesses();
+            foreach (Process process in processlist)
+            {
+                if (process.MainWindowTitle.Contains("Dofus"))
+                {
+                    // dofus.Add("Process: "+process.ProcessName +" ID: "+process.Id+" Window title: "+process.MainWindowTitle+"");
+                    Border border = new Border();
+                    CheckBox checkBox =  new CheckBox();
+                    checkBox.Margin = new System.Windows.Thickness { Left = 5, Top = 0, Right = 0, Bottom = 0 };
+                    checkBox.Content = process.MainWindowTitle;
+                    border.Child = checkBox;
+                    dofus.Add(process);
+                    this.dofusProcess.Children.Add(border);
+                }
+            }
         }
 
         private void ButtonAccount_OnClick(object sender, RoutedEventArgs e)
