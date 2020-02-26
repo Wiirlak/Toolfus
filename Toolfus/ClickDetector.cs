1af221af2a39cd7b27cfef9fa3c23687895df500
Desjardins Alexis <vizuar.gamer@gmail.com>
2020-02-26 18:37:59 +0100

    [Click] Handle click only in Dofus windows

--- Toolfus/ClickDetector.cs
+++ Toolfus/ClickDetector.cs
@@ -1,10 +1,5 @@
 ﻿using Gma.System.MouseKeyHook;
-using System;
 using System.Diagnostics;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace Toolfus
@@ -12,12 +7,14 @@ namespace Toolfus
     public class ClickDetector
     {
         private IKeyboardMouseEvents m_GlobalHook;
+        private WindowUtils wu;
         public void Subscribe()
         {
             // Note: for the application hook, use the Hook.AppEvents() instead
             m_GlobalHook = Hook.GlobalEvents();
+            wu = new WindowUtils();
 
-            m_GlobalHook.MouseDownExt += GlobalHookMouseDownExt;
+            m_GlobalHook.MouseUpExt += GlobalHookMouseUpExt;
             m_GlobalHook.KeyPress += GlobalHookKeyPress;
         }
 
@@ -26,18 +23,24 @@ namespace Toolfus
             Debug.WriteLine("KeyPress: \t{0}", e.KeyChar);
         }
 
-        private void GlobalHookMouseDownExt(object sender, MouseEventExtArgs e)
+        private void GlobalHookMouseUpExt(object sender, MouseEventExtArgs e)
         {
-            Debug.WriteLine("MouseDown: \t{0}; \t System Timestamp: \t{1}", e.Button, e.Timestamp);
+            if(wu.GetActiveProcessName().Equals("Dofus"))
+                Debug.WriteLine("MouseDown: \tX:{0} | Y:{1}", e.X, e.Y);
+            // Debug.WriteLine("Window : " + wu.GetActiveWindowTitle() );
+            // Debug.WriteLine("Process : " + wu.GetActiveProcessName() );
 
             // uncommenting the following line will suppress the middle mouse button click
             // if (e.Buttons == MouseButtons.Middle) { e.Handled = true; }
         }
+        
+        
+
 
         public void Unsubscribe()
         {
-            m_GlobalHook.MouseDownExt -= GlobalHookMouseDownExt;
             m_GlobalHook.KeyPress -= GlobalHookKeyPress;
+            m_GlobalHook.MouseUpExt -= GlobalHookMouseUpExt;
 
             //It is recommened to dispose it
             m_GlobalHook.Dispose();
